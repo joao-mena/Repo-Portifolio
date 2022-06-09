2e0f442307a7889e684dbdfdea21cc8fce8c87db
joao.mena <jsemeaomen@hotmail.com>
2022-06-08 23:00:24 -0300

    mini portifolio 100%

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,26 +1,26 @@
-let timeline = document.querySelector(".timeline-section");
-let items = timeline.querySelectorAll("li");
+// let projects = document.getElementById("projects");
+// let itemsProjects = projects.querySelectorAll("li");
 
-function isItemInView(item) {
-  let rect = item.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+// function isItemInView(item) {
+//   let rect = item.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <=
+//       (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
 
-function callbackFunc() {
-  for (let i = 0; i < items.length; i++) {
-    if (isItemInView(items[i])) {
-      items[i].classList.add("show");
-    }
-  }
-}
+// function callbackFunc(items) {
+//   for (let i = 0; i < items.length; i++) {
+//     if (isItemInView(items[i])) {
+//       items[i].classList.add("show");
+//     }
+//   }
+// }
 
-// listen for events
-window.addEventListener("load", callbackFunc);
-window.addEventListener("resize", callbackFunc);
-window.addEventListener("scroll", callbackFunc);
+// // listen for events
+// window.addEventListener("load", callbackFunc(itemsProjects));
+// window.addEventListener("resize", callbackFunc(itemsProjects));
+// window.addEventListener("scroll", callbackFunc(itemsProjects));
